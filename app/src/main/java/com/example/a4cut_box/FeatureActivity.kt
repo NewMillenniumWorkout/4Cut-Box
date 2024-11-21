c76055ad34524392ed8abf51deaf4b3643ad2eb4
minseok128 <minseok128128@gmail.com>
2024-11-21 20:00:40 +0900

    Added FeatureActivity.kt : 핵심 기능이 들어갈 곳 추가

---
+++ app/src/main/java/com/example/a4cut_box/FeatureActivity.kt
@@ -0,0 +1,18 @@
+// SecondActivity.kt
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+
+class FeatureActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Surface(color = MaterialTheme.colorScheme.background) {
+                Text(text = "Welcome to Second Activity!")
+            }
+        }
+    }
+}
