f71ad289f06f36bbbafdc67bc80dc6089a866ced
minseok128 <minseok128128@gmail.com>
2024-11-21 19:59:45 +0900

    Update SignInPage.kt : ElevatedButton으로 임시 로그인 성공하도록 구현

--- app/src/main/java/com/example/a4cut_box/auth/SignInPage.kt
+++ app/src/main/java/com/example/a4cut_box/auth/SignInPage.kt
@@ -1,14 +1,21 @@
 package com.example.a4cut_box.auth
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
 @Composable
-fun SignInPage(modifier: Modifier = Modifier, onClickSignUp: () -> Unit) {
-    Text("hello SignInPage")
-    ElevatedButton(onClick = onClickSignUp) {
-        Text("Sign Up")
+fun SignInPage(modifier: Modifier = Modifier, onClickSignUp: () -> Unit, onClickTmp: () -> Unit) {
+    Column(modifier = Modifier.fillMaxSize()) {
+        Text("hello SignInPage")
+        ElevatedButton(onClick = onClickSignUp) {
+            Text("Sign Up")
+        }
+        ElevatedButton(onClick = onClickTmp) {
+            Text("Sign In")
+        }
     }
 }
