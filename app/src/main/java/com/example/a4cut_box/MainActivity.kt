6bd13e107d80a85eed13952aaa7a3b276e194f33
minseok128 <minseok128128@gmail.com>
2024-11-21 20:21:00 +0900

    Update MainActivity.kt : 코드 분리

--- app/src/main/java/com/example/a4cut_box/MainActivity.kt
+++ app/src/main/java/com/example/a4cut_box/MainActivity.kt
@@ -1,33 +1,19 @@
 package com.example.a4cut_box
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.a4cut_box.auth.SignInPage
 import com.example.a4cut_box.auth.SignUpPage
-import com.example.a4cut_box.bottomBar.BottomBar
-import com.example.a4cut_box.calendar.CalendarPage
-import com.example.a4cut_box.camera.CameraPage
-import com.example.a4cut_box.camera.CameraSavePage
-import com.example.a4cut_box.home.HomePage
-import com.example.a4cut_box.map.MapPage
-import com.example.a4cut_box.photoDetail.PhotoDetailPage
-import com.example.a4cut_box.setting.SettingPage
 import com.example.a4cut_box.ui.theme._4CutBoxTheme
 
 
@@ -37,80 +23,31 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             val navController = rememberNavController()
-            var isSignIn by remember { mutableStateOf(false) }
+            val context = this@MainActivity
 
             _4CutBoxTheme {
-                if (!isSignIn) {
-                    Scaffold(
-                        modifier = Modifier.fillMaxSize()
-                    ) { innerPadding ->
-                        NavHost(
-                            modifier = Modifier.padding(innerPadding),
-                            navController = navController,
-                            startDestination = "SignIn"
-                        ) {
-                            composable("SignIn") {
-                                SignInPage(
-                                    onClickSignUp = { navController.navigate("SignUp") },
-                                    onClickTmp = {
-                                        isSignIn = true
-                                        navController.navigate("Home") {
-                                            popUpTo("SignIn") {
-                                                inclusive = true
-                                            } // SignIn을 제거하고 Home으로 이동
-                                        }
-                                    })
-                            }
-                            composable("SignUp") {
-                                SignUpPage(onClickBack = { navController.navigateUp() })
-                            }
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
+                    NavHost(
+                        navController = navController,
+                        startDestination = "SignIn",
+                        modifier = Modifier.padding(innerPadding)
+                    ) {
+                        composable("SignIn") {
+                            SignInPage(
+                                onClickSignUp = { navController.navigate("SignUp") },
+                                onClickTmp = {
+                                    val intent = Intent(context, FeatureActivity::class.java)
+                                    context.startActivity(intent)
+                                })
                         }
-                    }
-                } else {
-                    Scaffold(
-                        modifier = Modifier.fillMaxSize(),
-                        bottomBar = {
-                            BottomBar()
+                        composable("SignUp") {
+                            SignUpPage(onClickBack = { navController.navigateUp() })
                         }
-                    ) { innerPadding ->
-                        NavHostBody(
-                            innerPadding = innerPadding,
-                            navController = navController
-                        )
                     }
                 }
             }
-        }
-    }
-}
-
-@Composable
-fun NavHostBody(innerPadding: PaddingValues, navController: NavHostController) {
-    NavHost(
-        modifier = Modifier.padding(innerPadding),
-        navController = navController,
-        startDestination = "Home"
-    ) {
-        composable("Home") {
-            HomePage()
-        }
-        composable("calendar") {
-            CalendarPage()
-        }
-        composable("photoDetail") {
-            PhotoDetailPage()
-        }
-        composable("camera") {
-            CameraPage()
-        }
-        composable("cameraSave") {
-            CameraSavePage()
-        }
-        composable("map") {
-            MapPage()
-        }
-        composable("setting") {
-            SettingPage()
         }
     }
 }
