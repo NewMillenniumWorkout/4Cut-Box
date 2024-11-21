85c694bc88d2aa904303ad9cf17f963a53da1fdc
minseok128 <minseok128128@gmail.com>
2024-11-21 20:21:36 +0900

    Update FeatureActivity.kt : 기존 코드 이동 완료

--- app/src/main/java/com/example/a4cut_box/FeatureActivity.kt
+++ app/src/main/java/com/example/a4cut_box/FeatureActivity.kt
@@ -1,17 +1,67 @@
-// SecondActivity.kt
+package com.example.a4cut_box
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.a4cut_box.bottomBar.BottomBar
+import com.example.a4cut_box.calendar.CalendarPage
+import com.example.a4cut_box.camera.CameraPage
+import com.example.a4cut_box.camera.CameraSavePage
+import com.example.a4cut_box.home.HomePage
+import com.example.a4cut_box.map.MapPage
+import com.example.a4cut_box.photoDetail.PhotoDetailPage
+import com.example.a4cut_box.setting.SettingPage
+import com.example.a4cut_box.ui.theme._4CutBoxTheme
+
 
 class FeatureActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Surface(color = MaterialTheme.colorScheme.background) {
-                Text(text = "Welcome to Second Activity!")
+            val navController = rememberNavController()
+
+            _4CutBoxTheme {
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    bottomBar = {
+                        BottomBar()
+                    }
+                ) { innerPadding ->
+                    NavHost(
+                        modifier = Modifier.padding(innerPadding),
+                        navController = navController,
+                        startDestination = "Home"
+                    ) {
+                        composable("Home") {
+                            HomePage()
+                        }
+                        composable("calendar") {
+                            CalendarPage()
+                        }
+                        composable("photoDetail") {
+                            PhotoDetailPage()
+                        }
+                        composable("camera") {
+                            CameraPage()
+                        }
+                        composable("cameraSave") {
+                            CameraSavePage()
+                        }
+                        composable("map") {
+                            MapPage()
+                        }
+                        composable("setting") {
+                            SettingPage()
+                        }
+                    }
+                }
             }
         }
     }
