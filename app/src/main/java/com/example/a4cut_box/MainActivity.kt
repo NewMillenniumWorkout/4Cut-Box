b13ff2f4ef00d48694f09530c15ad51522a4bfb1
minseok128 <minseok128128@gmail.com>
2024-11-21 15:57:27 +0900

    Update MainActivity.kt : NavHost Setting 수행

--- app/src/main/java/com/example/a4cut_box/MainActivity.kt
+++ app/src/main/java/com/example/a4cut_box/MainActivity.kt
@@ -7,41 +7,44 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.a4cut_box.auth.LogInPage
+import com.example.a4cut_box.bottomBar.BottomBar
+import com.example.a4cut_box.home.HomePage
 import com.example.a4cut_box.ui.theme._4CutBoxTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val navController = rememberNavController()
+
             _4CutBoxTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    bottomBar = {
+                        BottomBar()
+                    }
+                ) { innerPadding ->
+                    NavHost(
+                        navController = navController,
+                        startDestination = "LogIn",
                         modifier = Modifier.padding(innerPadding)
-                    )
+                    ) {
+                        composable("LogIn") {
+                            LogInPage()
+                        }
+                        composable("Home") {
+                            HomePage()
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    _4CutBoxTheme {
-        Greeting("Android")
     }
 }
