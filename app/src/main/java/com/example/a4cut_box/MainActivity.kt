7bda4bc04090123eac4d844d83528fbef2d061c1
minseok128 <minseok128128@gmail.com>
2024-11-21 16:19:00 +0900

    Update MainActivity.kt : NavHostBody에 모든 페이지 명시

--- app/src/main/java/com/example/a4cut_box/MainActivity.kt
+++ app/src/main/java/com/example/a4cut_box/MainActivity.kt
@@ -4,16 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.a4cut_box.auth.LogInPage
+import com.example.a4cut_box.auth.SignInPage
+import com.example.a4cut_box.auth.SignUpPage
 import com.example.a4cut_box.bottomBar.BottomBar
+import com.example.a4cut_box.calendar.CalendarPage
+import com.example.a4cut_box.camera.CameraPage
+import com.example.a4cut_box.camera.CameraSavePage
 import com.example.a4cut_box.home.HomePage
+import com.example.a4cut_box.map.MapPage
+import com.example.a4cut_box.photoDetail.PhotoDetailPage
+import com.example.a4cut_box.setting.SettingPage
 import com.example.a4cut_box.ui.theme._4CutBoxTheme
 
 
@@ -31,20 +41,52 @@ class MainActivity : ComponentActivity() {
                         BottomBar()
                     }
                 ) { innerPadding ->
-                    NavHost(
-                        navController = navController,
-                        startDestination = "LogIn",
-                        modifier = Modifier.padding(innerPadding)
-                    ) {
-                        composable("LogIn") {
-                            LogInPage()
-                        }
-                        composable("Home") {
-                            HomePage()
-                        }
-                    }
+                    NavHostBody(
+                        innerPadding = innerPadding,
+                        navController = navController
+                    )
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun NavHostBody(innerPadding: PaddingValues, navController: NavHostController) {
+    NavHost(
+        navController = navController,
+        startDestination = "SignIn",
+        modifier = Modifier.padding(innerPadding)
+    ) {
+        composable("SignIn") {
+            SignInPage()
+        }
+        composable("SignUp") {
+            SignUpPage()
+        }
+        composable("Home") {
+            HomePage()
+        }
+        composable("calendar") {
+            CalendarPage()
+        }
+        composable("photoDetail") {
+            PhotoDetailPage()
+        }
+        composable("camera") {
+            CameraPage()
+        }
+        composable("cameraSave") {
+            CameraSavePage()
+        }
+        composable("map") {
+            MapPage()
+        }
+        composable("setting") {
+            SettingPage()
+        }
+        composable("map") {
+            MapPage()
         }
     }
 }
