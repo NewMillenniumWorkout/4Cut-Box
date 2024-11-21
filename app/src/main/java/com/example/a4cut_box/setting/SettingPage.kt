8ab8b65c3a038e2322b54b66e2f84fb9700e216c
minseok128 <minseok128128@gmail.com>
2024-11-21 23:10:05 +0900

    Update SettingPage.kt : 로그아웃 구현, MainActivity로 이동

--- app/src/main/java/com/example/a4cut_box/setting/SettingPage.kt
+++ app/src/main/java/com/example/a4cut_box/setting/SettingPage.kt
@@ -1,10 +1,29 @@
 package com.example.a4cut_box.setting
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.google.firebase.auth.FirebaseAuth
 
 @Composable
-fun SettingPage(modifier: Modifier = Modifier) {
-    Text("hello SettingPage")
+fun SettingPage(
+    modifier: Modifier = Modifier,
+    goToMainActivity: () -> Unit
+) {
+    Column(modifier = Modifier.fillMaxSize()) {
+        Text("hello SettingPage")
+        Button(onClick = { signOutUser(goToMainActivity) }) {
+            Text("Sign Out")
+        }
+    }
+}
+
+fun signOutUser(onSuccess: () -> Unit) {
+    val auth = FirebaseAuth.getInstance()
+    
+    auth.signOut()
+    onSuccess()
 }
