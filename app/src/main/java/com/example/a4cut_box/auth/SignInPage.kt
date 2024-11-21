714cee4cf0f69852aba88ed9e39354af1386ef45
minseok128 <minseok128128@gmail.com>
2024-11-21 16:33:17 +0900

    Update SignInPage.kt : SignUp으로 넘어가는 버튼 임시 추가

--- app/src/main/java/com/example/a4cut_box/auth/SignInPage.kt
+++ app/src/main/java/com/example/a4cut_box/auth/SignInPage.kt
@@ -1,10 +1,14 @@
 package com.example.a4cut_box.auth
 
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
 @Composable
-fun SignInPage(modifier: Modifier = Modifier) {
+fun SignInPage(modifier: Modifier = Modifier, onClickSignUp: () -> Unit) {
     Text("hello SignInPage")
+    ElevatedButton(onClick = onClickSignUp) {
+        Text("Sign Up")
+    }
 }
