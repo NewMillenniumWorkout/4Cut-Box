973dae3ff5977fac878366c3e733bf614892455e
minseok128 <minseok128128@gmail.com>
2024-11-22 13:10:25 +0900

    Update CameraPage.kt : 카메라 페이지에 카메라 띄우기 완료

--- app/src/main/java/com/example/a4cut_box/camera/CameraPage.kt
+++ app/src/main/java/com/example/a4cut_box/camera/CameraPage.kt
@@ -1,10 +1,9 @@
 package com.example.a4cut_box.camera
 
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
 @Composable
 fun CameraPage(modifier: Modifier = Modifier) {
-    Text("hello CameraPage")
+    CameraScreen()
 }
