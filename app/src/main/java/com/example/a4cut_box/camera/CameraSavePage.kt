45d5b6eaa9edac83a0c014f7889a623f5868f8bf
minseok128 <minseok128128@gmail.com>
2024-11-26 16:47:48 +0900

    Update CameraSavePage.kt : 이미지 키우기 성공

--- app/src/main/java/com/example/a4cut_box/camera/CameraSavePage.kt
+++ app/src/main/java/com/example/a4cut_box/camera/CameraSavePage.kt
@@ -1,7 +1,11 @@
 package com.example.a4cut_box.camera
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
@@ -12,12 +16,16 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.a4cut_box.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -48,10 +56,22 @@ fun CameraSavePage(modifier: Modifier = Modifier, onClickBack: () -> Unit) {
     ) { innerPadding ->
         Column(
             modifier = Modifier
-                .padding(innerPadding)
                 .fillMaxSize()
+                .padding(innerPadding),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Top
         ) {
-
+            Image(
+                painter = painterResource(id = R.drawable.photo_example), "photo",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .aspectRatio(1f), // 이미지가 비율에 맞게 조정되도록 설정
+            )
+            TextField(
+                value = "",
+                modifier = Modifier.fillMaxWidth(),
+                onValueChange = {}
+            )
         }
     }
 }
