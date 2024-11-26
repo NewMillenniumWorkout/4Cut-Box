b85bd5efc68591d7a3d64a3223ab9807c4decb0d
김민식 <minsik@gimminsig-ui-MacBookPro.local>
2024-11-26 19:02:14 +0900

    Update BottomBar.kt : Add IconButtonWithLabel fun

--- app/src/main/java/com/example/a4cut_box/bottomBar/BottomBar.kt
+++ app/src/main/java/com/example/a4cut_box/bottomBar/BottomBar.kt
@@ -5,10 +5,15 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DateRange
@@ -17,16 +22,25 @@ import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Place
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.BlurredEdgeTreatment
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.blur
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.a4cut_box.R
 import com.example.a4cut_box.ui.theme.BoxBlack
 
@@ -38,12 +52,28 @@ fun BottomBar(
     onClickSetting: () -> Unit
 ) {
     Box(Modifier.fillMaxWidth()) {
+
         Image(
             painter = painterResource(id = R.drawable.bottom_box),
             contentDescription = "Bottom AppBar Background",
             modifier = Modifier
                 .fillMaxWidth()
-                .height(64.dp)
+                .height(80.dp)
+                .align(Alignment.BottomCenter)
+                .alpha(0.3f)
+                .blur(
+                    radius = 4.dp,
+                    edgeTreatment = BlurredEdgeTreatment.Unbounded
+                ),
+            contentScale = ContentScale.FillBounds,
+            colorFilter = ColorFilter.tint(BoxBlack)
+        )
+        Image(
+            painter = painterResource(id = R.drawable.bottom_box),
+            contentDescription = "Bottom AppBar Background",
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(80.dp)
                 .align(Alignment.BottomCenter),
             contentScale = ContentScale.FillBounds
         )
@@ -53,46 +83,75 @@ fun BottomBar(
             contentColor = MaterialTheme.colorScheme.primary,
             actions = {
                 Row(
-                    modifier = Modifier.fillMaxSize(),
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(horizontal = 4.dp),
                     verticalAlignment = Alignment.CenterVertically,
                     horizontalArrangement = Arrangement.SpaceBetween
                 ) {
-                    Row(
-                        modifier = Modifier.fillMaxWidth()
-                    ) {
-                        IconButton(onClick = onClickHome) {
-                            Icon(
-                                painter = painterResource(R.drawable.home),
-                                contentDescription = "Home",
-                                tint = BoxBlack
-                            )
-                        }
-                        IconButton(onClick = onClickCalendar) {
-                            Icon(
-                                painter = painterResource(R.drawable.calendar_days),
-                                contentDescription = "Calendar",
-                                tint = BoxBlack
-                            )
-                        }
-                    }
-                    Row {
-                        IconButton(onClick = onClickMap) {
-                            Icon(
-                                painter = painterResource(R.drawable.map),
-                                contentDescription = "Map",
-                                tint = BoxBlack
-                            )
-                        }
-                        IconButton(onClick = onClickSetting) {
-                            Icon(
-                                painter = painterResource(R.drawable.settings_8),
-                                contentDescription = "Setting",
-                                tint = BoxBlack
-                            )
-                        }
-                    }
+                    IconButtonWithLabel(
+                        onClick = onClickHome,
+                        icon= R.drawable.home,
+                        description = "Home",
+                        label = "홈",
+                        color = BoxBlack
+                    )
+                    IconButtonWithLabel(
+                        onClick = onClickCalendar,
+                        icon= R.drawable.calendar_days,
+                        description = "Calendar",
+                        label = "달력",
+                        color = BoxBlack
+                    )
+                    Spacer(modifier = Modifier.size(56.dp))
+                    IconButtonWithLabel(
+                        onClick = onClickMap,
+                        icon= R.drawable.map,
+                        description = "Map",
+                        label = "지도",
+                        color = BoxBlack
+                    )
+                    IconButtonWithLabel(
+                        onClick = onClickSetting,
+                        icon= R.drawable.settings_8,
+                        description = "Setting",
+                        label = "설정",
+                        color = BoxBlack
+                    )
                 }
             }
         )
+    }
+}
+
+@Composable
+fun IconButtonWithLabel(
+    onClick: () -> Unit,
+    icon: Int,
+    description: String,
+    label: String,
+    color: Color,
+){
+    Button(
+        onClick = onClick,
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color.Transparent,
+            contentColor = BoxBlack
+        )
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Icon(
+                modifier = Modifier.size(28.dp),
+                painter = painterResource(icon),
+                contentDescription = description,
+                tint = color
+            )
+            Text(
+                fontSize = 12.sp,
+                text = label
+            )
+        }
     }
 }
