004adfb5402160db6bd87dc6397c9e3bb80b90ca
yeojin <yeojin7010+L@gmail.com>
2024-12-07 21:11:11 +0900

    Update FeatureActivity: Scaffold 기본 색 하얀색

--- app/src/main/java/com/example/a4cut_box/FeatureActivity.kt
+++ app/src/main/java/com/example/a4cut_box/FeatureActivity.kt
@@ -19,6 +19,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
@@ -47,6 +48,7 @@ class FeatureActivity : ComponentActivity() {
 
             _4CutBoxTheme {
                 Scaffold(
+                    containerColor = Color.White,
                     modifier = Modifier.fillMaxSize(),
                     floatingActionButtonPosition = FabPosition.Center,
                     floatingActionButton = {
