15f2e5d1124aa4df414a27eef96e4c733846695d
minseok128 <minseok128128@gmail.com>
2024-12-08 17:44:59 +0900

    Update Project : firebase realtime db 접근 허용

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,6 +62,7 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.firebase.auth.ktx)
+    implementation(libs.firebase.database.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
