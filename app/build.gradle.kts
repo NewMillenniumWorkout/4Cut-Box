1407d8f02c25c0099edbceb036959075903ccf4e
minseok128 <minseok128128@gmail.com>
2024-12-08 19:59:07 +0900

    Update Project : 위치 정보를 위해 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,6 +76,7 @@ dependencies {
     implementation(libs.androidx.navigation.compose)
     implementation(libs.firebase.auth.ktx)
     implementation(libs.firebase.database.ktx)
+    implementation(libs.play.services.location)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
