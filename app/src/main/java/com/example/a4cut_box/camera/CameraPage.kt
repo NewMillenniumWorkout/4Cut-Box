c423a22e1156ca5fe21ed2d8bee2b10eb2dccbd9
minseok128 <minseok128128@gmail.com>
2024-12-09 13:07:57 +0900

    Update CameraPage : 실험 전, 코드 완성

--- app/src/main/java/com/example/a4cut_box/camera/CameraPage.kt
+++ app/src/main/java/com/example/a4cut_box/camera/CameraPage.kt
@@ -13,6 +13,10 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -32,24 +36,31 @@ fun CameraPage(modifier: Modifier = Modifier, goToCameraSavePage: (image: String
                 Toast.makeText(context, "이미지를 선택하지 않았습니다.", Toast.LENGTH_SHORT).show()
             }
         }
+    var qrCode by remember { mutableStateOf("") }
 
     Box(modifier = Modifier.fillMaxSize()) {
-        CameraScreen()
-        Image(
-            painter = painterResource(id = R.drawable.qr_square), "qr",
-            modifier = Modifier
-                .align(Alignment.Center)
-                .size(250.dp)
-        )
-        ElevatedButton(
-            onClick = {
-                launcher.launch("image/*")
-            },
-            modifier = Modifier
-                .align(Alignment.BottomCenter)
-                .padding(vertical = 32.dp)
-        ) {
-            Text("직접 사진 선택")
+        if (qrCode.isNotEmpty()) {
+            Text(
+                text = "QR 코드 내용: $qrCode",
+            )
+        } else {
+            CameraScreen { scannedCode -> qrCode = scannedCode }
+            Image(
+                painter = painterResource(id = R.drawable.qr_square), "qr",
+                modifier = Modifier
+                    .align(Alignment.Center)
+                    .size(250.dp)
+            )
+            ElevatedButton(
+                onClick = {
+                    launcher.launch("image/*")
+                },
+                modifier = Modifier
+                    .align(Alignment.BottomCenter)
+                    .padding(vertical = 32.dp)
+            ) {
+                Text("직접 사진 선택")
+            }
         }
     }
 }
